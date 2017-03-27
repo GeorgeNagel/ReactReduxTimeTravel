cf9cafcfeb0721c6a2b2913b34db23e429226ed7
George Nagel <gnagel@datto.com>
2017-03-27 11:26:05 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+  entry: {
+    'app': [
+      'react-hot-loader/patch',
+      APP_DIR + '/index.js'
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    publicPath: "assets/",
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
